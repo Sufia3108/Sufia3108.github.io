101f9a0c53d4ef4c56416bd537be1992d7072fcd
Frog <sufia3108@gmail.com>
2025-09-11 12:24:17 -0600

    02 User Events

---
+++ 02 User Events/sketch.js
@@ -0,0 +1,45 @@
+// User Events + Day 1 Challenge
+// Sufia Chowdhury
+// September 11, 2025
+
+// GLOBAL VARIABLES HERE...
+let circleColor = false;
+//  declaration   initialization
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  challenge() // coordinate system challenge
+}
+
+function keyPressed(){
+  // this is a special EVENT function, gets
+  // automatically called anytime a keyboard
+  // button is pressed
+  print("key was pressed");
+  circleColor = !circleColor;
+
+  // how to tell WHICH key was pressed???
+}
+
+function challenge(){
+  // draw 5 hollow circles, in 4 corners and
+  // center position
+  noFill();
+  
+  if(circleColor){ //circleColor === true
+    fill(255, 0, 0);
+  }
+
+  // 5 circles
+  circle(width/2, height/2, 50)
+
+  circle(0, 0, 50)
+  circle(width, 0, 50)
+
+  circle(0, height, 50)
+  circle(width, height, 50)
+}
